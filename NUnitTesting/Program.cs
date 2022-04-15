e4208c7fce666eb889584d6afc35d462e8b149d0
Vidyashree N M <vidyashree2109@gmail.com>
2022-04-16 04:57:43 +0530

    [Vidya] Add : Temperature Conversion Is Implemented

--- NUnitTesting/Program.cs
+++ NUnitTesting/Program.cs
@@ -8,10 +8,10 @@ namespace NUnitTesting
         {
             Console.WriteLine("----------Welcome To N Unit Testing Programs----------");
             bool finish = true;
-            Console.WriteLine("\n1.Displaying Day Of Week\n2.End The Program");
+            Console.WriteLine("\n1.Displaying Day Of Week\n2.Temperature Conversion\n3.End The Program");
             while (finish)
             {
-                Console.WriteLine("\nEnter an option to execute :");
+                Console.Write("\nEnter an option to execute :");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -20,6 +20,11 @@ namespace NUnitTesting
                         day.PrintDay();
                         break;
                     case 2:
+                        TemperatureConversion temp = new TemperatureConversion();
+                        temp.CtoF();
+                        temp.FtoC();
+                        break;
+                    case 3:
                         finish = false;
                         break;
 
---
+++ NUnitTesting/TemperatureConversion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTesting
+{
+    public class TemperatureConversion
+    {
+        public void CtoF()
+        {
+            float farenheit, celcius;
+            Console.Write("Enter The Temprature In Celcius :");
+            celcius= float.Parse(Console.ReadLine());
+            farenheit = (celcius * 9 / 5) + 32;
+            Console.WriteLine("Temprature --> Celsius To Fahrenheit :" + farenheit + "°F");
+        }
+        public void FtoC()
+        {
+            float cel, faren;
+            Console.Write("\nEnter The Temprature In Fahrenheit :");
+            faren = float.Parse(Console.ReadLine());
+            cel = (faren - 32) * 5 / 9;
+            Console.WriteLine("Temprature --> Fahrenheit To Celsius :" + cel + "°C");
+        }
+    }
+}
